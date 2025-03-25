bf7ed606f4fd4f8170804f5ef24467ca2b2e615c
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-03-25 18:57:19 +0200

    Add Number, String

--- validate.py
+++ validate.py
@@ -30,13 +30,50 @@ class OneOf(Validator):
                 f'Expected {value!r} to be one of {self.options!r}')
 
 
-class Component:
-    kind = OneOf('wood', 'metal', 'plastic')
+class Number(Validator):
+    def __init__(self, minvalue=None, maxvalue=None):
+        self.minvalue = minvalue
+        self.maxvalue = maxvalue
 
-    def __init__(self, kind):
+    def validate(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError(f'Expected {value!r} is int or float')
+        if self.minvalue is not None and value < self.minvalue:
+            raise ValueError(f'Expected {value!r} is >= than {self.minvalue}')
+        if self.maxvalue is not None and self.maxvalue < value:
+            raise ValueError(f'Expected {value!r} <= than {self.maxvalue!r}')
+
+
+class String(Validator):
+    def __init__(self, minsize, maxsize, predicate):
+        self.minsize = minsize
+        self.maxsize = maxsize
+        self.predicate = predicate
+
+    def validate(self, value):
+        if not isinstance(value, str):
+            raise TypeError(f'Expected {value!r} is of type str')
+        if len(value) < self.minsize:
+            raise ValueError(
+                f'Expected {value!r} is not shorter than {self.minsize!r}')
+        if self.maxsize < len(value):
+            raise ValueError(
+                f'Expected {value!r} is not longer than {self.maxsize!r}')
+        if not self.predicate(value):
+            raise ValueError(
+                f'Expected {value!r} is {self.predicate.__name__}')
+
+
+class Component:
+    name = String(3, 9, str.isupper)
+    kind = OneOf('wood', 'metal', 'plastic')
+    quantity = Number(minvalue=0, maxvalue=50)
+
+    def __init__(self, name, kind, quantity):
+        self.name = name
         self.kind = kind
+        self.quantity = quantity
 
 
 if __name__ == '__main__':
-    Component('wood')
-    Component('glass')
+    Component('a', 'wood', 10)
