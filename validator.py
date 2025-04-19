ad8ded3fc6209e80d153097ec39652e989c66c71
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-04-19 20:17:29 +0300

    Initial import
    The Validator class from Raymond Hettinger. I typed it by heart with
    possible changes which I will fix when debugging.

---
+++ validator.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+"""
+>>> Component('wood', 10, 'FOO')
+"""
+from abc import ABC, abstractmethod
+
+
+class Validator(ABC):
+    def __set_name__(self, owner, name):
+        self.private_name = "_" + name
+        setattr(self, self.private_name, name)
+
+    def __get__(self, obj, objtype=None):
+        return getattr(obj, self.private_name)
+
+    def __set__(self, obj, value):
+        self.validate(value)
+        setattr(obj, self.private_name, value)
+
+    @abstractmethod
+    def validate(self, value):
+        pass
+
+
+class OneOf(Validator):
+    def __init__(self, *options):
+        self.options = options
+
+    def validate(self, value):
+        if value not in self.options:
+            raise ValueError(f"Expected {value!r} be one of {self.options}")
+
+
+class Number(Validator):
+    def __init__(self, minvalue=0, maxvalue=None):
+        self.minvalue = minvalue
+        self.maxvalue = maxvalue
+
+    def validate(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError(f"Expected {value!r} of type int or float")
+        if value < self.minvalue:
+            raise ValueError(f"Expected {value!r} >= {self.minvalue}")
+        if self.maxvalue < value:
+            raise ValueError(f"Expected {value!r} <= {self.maxvalue}")
+
+
+class String(Validator):
+    def __init__(self, minsize, maxsize, predicate):
+        self.minsize = minsize
+        self.maxsize = maxsize
+        self.predicate = predicate
+
+    def validate(self, value):
+        if len(value) < self.minsize:
+            raise ValueError(f"Expected len({value!r}) >= {self.minsize}")
+        if len(value) > self.maxsize:
+            raise ValueError(f"Expected len({value!r}) <= {self.maxsize}")
+        if not self.predicate(value):
+            raise ValueError(f"Expected {self.predicate}({value}) is true")
+
+
+class Component:
+    kind = OneOf("wood", "metal", "plastic")
+    quantity = Number(3, 25)
+    name = String(3, 10, str.isupper)
+
+    def __init__(self, kind, quantity, name):
+        self.kind = kind
+        self.quantity = quantity
+        self.name = name
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
