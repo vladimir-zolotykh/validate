a621929c8e6a77705ff4a1f7133470497c796d6a
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-04-22 09:15:46 +0300

    Initial import
    Classes OneOf0, Number0, String0

---
+++ validator_data.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+from abc import ABC, abstractmethod
+
+
+class Validator(ABC):
+    def __set_name__(self, owner, name):
+        self.private_name = "_" + name
+        setattr(self, self.private_name, name)
+
+    def __get__(self, obj, objtype=None):
+        return getattr(obj, self.private_name)
+
+    def __set__(self, obj, value):
+        self.validate(value)
+        setattr(obj, self.private_name, value)
+
+    @abstractmethod
+    def validate(self, value):
+        pass
+
+
+class OneOf0(Validator):
+    def __init__(self, *options):
+        self.options = options
+
+
+class Number0(Validator):
+    def __init__(self, minvalue=0, maxvalue=None):
+        self.minvalue = minvalue
+        self.maxvalue = maxvalue
+
+
+class String0(Validator):
+    def __init__(self, minsize, maxsize, predicate):
+        self.minsize = minsize
+        self.maxsize = maxsize
+        self.predicate = predicate
